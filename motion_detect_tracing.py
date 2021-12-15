cff81847cc2342cce9c5ee077fd3efbe4868b754
VItchanJin <64739304+VItchanJin@users.noreply.github.com>
2021-12-15 17:28:00 +0900

    Update motion_detect_tracing.py

--- motion_detect_tracing.py
+++ motion_detect_tracing.py
@@ -3,13 +3,6 @@ import numpy as np
 import argparse
 import time
 import datetime
-
-'''
-현재 설정
-fps=60
-터미널 or cmd에서 python motion_detect_tracing.py 으로 하면 연결된 카메라로 detect_tracing 수행
-raspberry 경로 재설정 필요
-'''
 a, b, c = None, None, None
 
 #argument parser 구성 및 분석
@@ -18,30 +11,28 @@ ap.add_argument("-v", "--video", help="path to the video file")
 ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
 args = vars(ap.parse_args())
 
-count = 0
-
 #video argument is None --> find conected cam
 if args.get("video",None) is None:
     camera = cv2.VideoCapture(0)
-    print("Cmera conected!!")
+    print("Camera conected!!")
     time.sleep(0.25)
     #video fps stting --> 적용 안되는거같음 찾아봐야함
-    camera.set(cv2.CAP_PROP_FPS, 30)
+    camera.set(cv2.CAP_PROP_FPS, 20)
 else:
     #otherwise, reading from video file
+    print("Camera disconected!!")
     camera = cv2.VideoCapture(args["video"]) 
  
 if camera.isOpened():
     ret, a = camera.read()
     ret, b = camera.read()
-
+    count = 0
     while ret:   
         fps = camera.get(cv2.CAP_PROP_FPS)     
         print(fps)
         count = count + 1 
         ret, c = camera.read()
-        draw = c.copy()        
-
+        draw = c.copy()      
         if not ret:
             break
         #연산속도를 높이기 위해 그레이스케일링 영상으로 변환
@@ -63,56 +54,46 @@ if camera.isOpened():
         #모노폴로지 연산을 위한 구조요소 생성
         kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
         #모노폴로지 연산을 통해 노이즈 제거 및 선명도 상승
-        diff = cv2.morphologyEx(diff, cv2.MORPH_OPEN, kernel)
-        
-        count2 = 0 
-
+        diff = cv2.morphologyEx(diff, cv2.MORPH_OPEN, kernel) 
         diff_cnt = cv2.countNonZero(diff)
+        count2 = 0
 
         if diff_cnt > 5:
-
-            count2 = count2 + 1            
-
+            count2 = count2 + 1   
             #nzero: diff는 카메라 영상과 사이즈가 같으며, a, b프레임의 차이 어레이를 의미함. 즉 0이 아닌 index을 return
             nzero = np.nonzero(diff)
+            #(min(nzero[1]), min(nzero[0]): diff에서 0이 아닌 값 중 행, 열이 가장 작은 포인트
+            #(max(nzero[1]), max(nzero[0]): diff에서 0이 아닌 값 중 행, 열이 가장 큰 포인트
             x = min(nzero[1])
             y = min(nzero[0])
             w = max(nzero[1]) - x
             h = max(nzero[0]) - y
-
-
             #rectangle: pt1(min(nzero[1]),min(nzero[0])), pt2(max(nzero[1]), max(nzero[0]) 기준으로 사각형 프레임을 만들어줌.            
-            cv2.rectangle(draw, (x, y),(w, h), (0, 255, 0), 2)               
-            #(min(nzero[1]), min(nzero[0]): diff에서 0이 아닌 값 중 행, 열이 가장 작은 포인트
-            #(max(nzero[1]), max(nzero[0]): diff에서 0이 아닌 값 중 행, 열이 가장 큰 포인트
+            cv2.rectangle(draw, (x, y),(w, h), (0, 255, 0), 2)                         
             #(0, 255, 0): 사각형을 그릴 색상 값 #2 : thickness          
-
-            #imgwrite area
-            img_trim=draw[y:y+h, x:x+w]                        
-            
+            #             
             cv2.putText(draw, "Motion detected!!", (10, 30),
                         cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 255))
-
             #save image
-            cv2.imwrite("D:/VMD_test_image/frame%d_%d.jpg"%(count, count2),img_trim)
+            cv2.imwrite("detect_capture/detect_img_%d_%d.jpg"%(count, count2),draw)
 
         #영상을 원본으로 복구(배열을 다시 앞으로 붙임,그레이스케일을 BGR 색상 이미지로 반환)
         stacked = np.hstack((draw, cv2.cvtColor(diff, cv2.COLOR_GRAY2BGR)))
-        cv2.putText(stacked, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),(25, stacked.shape[0]-50), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+        cv2.putText(stacked, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+            (25, stacked.shape[0]-50), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
         cv2.imshow('Motion_detect_tracing', stacked)
-        
-        #video recording save               
-        inputVideo = 'D:/VMD_test_video/video.avi' 
-        w = round(camera.get(cv2.CAP_PROP_FRAME_WIDTH))          
-        h = round(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-        outVideo = cv2.VideoWriter(inputVideo, fourcc, fps, (w,h))  
-        stacked = cv2.resize(stacked,(w,h))  
-        outVideo.write(stacked)
- 
-        a = b
-        b = c
 
+        #video recording save                     
+        #outVideo = 'C:/Users/daseul.lee/Downloads/NOIRcamera-main/detect_capture/video.avi' 
+        #w = round(camera.get(cv2.CAP_PROP_FRAME_WIDTH))          
+        #h = round(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        #stacked = cv2.resize(stacked,(w,h))
+        #fourcc = cv2.VideoWriter_fourcc(*'XVID')              
+        #outVideo = cv2.VideoWriter(outVideo, fourcc, fps, (w,h))
+        #outVideo.write(stacked)   
+        
+        a = b
+        b = c     
         key = cv2.waitKey(1) & 0xFF
         #종료를 위한 명령어 "q"
         if key ==ord("q"):
